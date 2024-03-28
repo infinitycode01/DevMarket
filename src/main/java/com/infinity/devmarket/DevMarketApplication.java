6189c134cdefde56f480c00d34a56f76e5b1275b
dmytro <infinity.code10@gmail.com>
2024-03-28 15:46:14 +0200

    First commit

---
+++ src/main/java/com/infinity/devmarket/DevMarketApplication.java
@@ -0,0 +1,13 @@
+package com.infinity.devmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/infinity/devmarket/DevMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infinity.devmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
